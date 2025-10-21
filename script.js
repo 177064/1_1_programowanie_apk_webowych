5d8e1327cbb02dcdb7c9e8a9b2a13e6beb9e520c
177064 <177064@stud.prz.edu.pl>
2025-10-21 18:09:23 +0000

    C1_2.1 ex3 drag&drop

--- script.js
+++ script.js
@@ -21,19 +21,17 @@
 
   function number() {
 
-    //ex2_content.innerHTML = "test";
-
     if (ex2_text.value.length != 9) {
-       ex2_content.innerHTML = "Długość numeru musi być równa 9";
+      ex2_content.innerHTML = "Długość numeru musi być równa 9";
     }
     else if (ex2_text.value.match(/[A-Z|a-z]/)) {
       ex2_content.innerHTML = "Numer nie może zawierać liter";
     }
     else if (ex2_text.value.match(/[^0-9]/)) {
-        ex2_content.innerHTML = "Numer nie może zawierać znaków specjalnych";
+      ex2_content.innerHTML = "Numer nie może zawierać znaków specjalnych";
     }
     else if (ex2_text.value.match(/[0-9]/) && ex2_text.value.length == 9) {
-        ex2_content.innerHTML = "Numer telefonu jest poprawny";
+      ex2_content.innerHTML = "Numer telefonu jest poprawny";
     }
     else {
       ex2_content.innerHTML = "Numer telefonu nie jest poprawny";
@@ -41,7 +39,25 @@
 
   }
 
+  // zad3
+  const ex3_e = document.getElementById("ex3_element");
+  const ex3_two = document.getElementById("ex3_two");
+  ex3_e.addEventListener("dragstart", dragstartHandler);
+  ex3_two.addEventListener("drop", dropHandler);
+  ex3_two.addEventListener("dragover", dragoverHandler);
 
+  function dragstartHandler(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
 
+  function dragoverHandler(ev) {
+    ev.preventDefault();
+  }
+
+  function dropHandler(ev) {
+    ev.preventDefault();
+    const data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
 
 })();
